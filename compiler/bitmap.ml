eb1e9a03b333a001cf89f30b3794918dc4a54168
jxt400 <jxt400@0@cs.bham.ac.uk>
2017-02-13 11:16:05 +0000

    Added simple graph search

--- compiler/bitmap.ml
+++ compiler/bitmap.ml
@@ -1,4 +1,6 @@
-let adj_list = Hashtbl.create 10000
+(* TODO calculate coordinates of ports *)
+
+let graph = Hashtbl.create 10000
 
 let xLoc = ref 0
 let yLoc = ref 0
@@ -10,10 +12,12 @@ type node = {
     name : string;
     xLoc : int;
     yLoc : int;
+    status : int;
     successors : string list; (* might be better as a hashtable *)
 }
 
 (* Priorities -
+      0       - Free
       1       - Taken by horizontal wire
       2       - Taken by vertical wire
       3       - Taken by wire corner or box
@@ -52,33 +56,64 @@ let generate_adjacency_lists x y =
                         (* somewhere in the centre of the grid *)
                         [(string_of_coord (x'-1) y); (string_of_coord (x'-1) (y'-1)); (string_of_coord x' (y'-1)); (string_of_coord (x'+1) (y'-1)); (string_of_coord (x'+1) y'); (string_of_coord (x'+1) (y'+1)); (string_of_coord x' (y'+1)); (string_of_coord (x'-1) (y'+1))]
       ) in
-      Hashtbl.add adj_list n {
+      Hashtbl.add graph n {
                                 name = n;
                                 xLoc = i;
                                 yLoc = j;
+                                status = 0;
                                 successors = succ;
                               }
     done;
   done
 
-(* Takes a list of morphisms and produces a graph *)
-let rec place_morphisms box_size bitmap = function
-  | []              -> bitmap
+let block_coord coord x y =
+  let {name, xL, yL, status, succ} = Hashtbl.find graph coord in
+  Hashtbl.replace graph coord {name, xL, yL, 3, succ}
+
+(* box_size -> morphism_list -> unit *)
+let rec place_morphisms box_size = function
+  | []              -> ()
   | (m, (x,y))::xs  ->
-    
-
-
-    (*
-    (* assume x and y have been scaled *)
-    (* box_size is the number of squares that the box should occupy *)
-      let mx = m.(x) in
-      mx.(y) <- 3;
-      (* this represents the left-bottom aligned origin of the box *)
-      (* thus we want to loop right and up (it doesn't matter that our array indexes will be flipped) *)
-      for i = x to (x+box_size) do
-        for j = y to (y+box_size) do
-          let mi = m.(i) in
-          mi.(j) <- 3;
-        done;
+    (* assume x y have been scaled and represent the left-bottom-aligned origin of the box *)
+    (* now we mark the every node as occupied in the hashtable *)
+    for i = x to (x+box_size) do
+      for j = y to (y+box_size) do
+          string_of_coord x y |> block_coord i j  (* type unit *)
       done;
-      place_morphisms box_size bitmap xs (* xs is the list of morphisms *) *)
+    done
+
+let rec expand vertex = let {_,_,_,_,succ} = Hashtbl.find graph vertex in
+                        succ
+
+let strategy oldf newf = newf @ oldf (* TODO - change this to priority queue *)
+
+
+let rec search start goal fringe path = match fringe with
+  | []    -> failwith "No route exists"
+  | ({name,x,y,status,succ})::xs -> if goal == name then [(x,y)]
+                                    else
+                                      (match status with
+                                        | 0 -> succ
+                                        | 1 -> (* can only go up/down *)
+                                        | 2 -> (* can only go left/right *)
+                                        | 3 -> [] )
+
+(* TODO - add heuristics *)
+let rec search fringe start goal path =
+  match fringe with
+    | []    -> failwith "No route exists"
+    | x::xs -> if goal x then [x]
+               else search graph (strategy xs (expand x graph) goal (path@[x])
+
+let rec find_route = function
+  | []           -> [[]]
+  | (out,in)::xs -> (search [] src dest []) :: find_route xs
+
+let find_routes wires width height boxes =
+  let box_size = width /. float(boxes |> List.length)
+  and width'   = width * 200
+  and height'  = height * 200
+  in
+  generate_adjacency_lists width' height';
+  place_morphisms box_size boxes;
+  paths (* TODO: scale back to tikz scheme *)
